4187b574dde83a549314240ebbe18e2f96a0ee53
sebasvega95 <sebas060495@gmail.com>
2017-08-09 13:29:21 -0500

    added surf homography test

--- feature_matching.py
+++ feature_matching.py
@@ -1,8 +1,15 @@
 import cv2
-from tests import test_tbmatch
+from tests import test_tbmatch, test_surfmatch
 
 if __name__ == '__main__':
     ref_img = cv2.imread('test-images/monster1s.JPG', 0)
     query_img = cv2.imread('test-images/monster1s.rot.JPG', 0)
+
     tb_inliners = test_tbmatch(ref_img, query_img)
-    print('% inliers {:.3f}%'.format(tb_inliners * 100))
+    surf_inliers = test_surfmatch(ref_img, query_img)
+
+    print('Homography test')
+    print('---------------\n')
+    print('% inliers')
+    print('Triangle-based: {:.3f}%'.format(tb_inliners * 100))
+    print('SURF:           {:.3f}%'.format(surf_inliers * 100))
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,2 @@
 from .tm_test import test_tbmatch
+from .surf_test import test_surfmatch
